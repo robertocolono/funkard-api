bf8b9548f6a97827aae4ce907ab7a72e529f68f5
robertocolono <roberto.colono17@gmail.com>
2025-10-13 20:26:56 +0000

    fix: add missing authentication imports

--- src/main/java/com/funkard/config/SecurityConfig.java
+++ src/main/java/com/funkard/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.funkard.config;
 import com.funkard.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
