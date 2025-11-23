e5d404138998007e121a4f5933ef9a3213264dcb
robertocolono <roberto.colono17@gmail.com>
2025-11-23 14:47:25 +0100

    fix: aggiunto import List mancante in AdminUserRepository

--- src/main/java/com/funkard/adminauth/AdminUserRepository.java
+++ src/main/java/com/funkard/adminauth/AdminUserRepository.java
@@ -3,6 +3,7 @@ package com.funkard.adminauth;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
